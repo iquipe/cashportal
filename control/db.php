89eedb6b14c60441e25763186fe65509a210337e
Andrew Quaye <andyquayegh@outlook.com>
2018-12-06 08:34:07 +0100

    add connection
    add connection

--- control/db.php
+++ control/db.php
@@ -16,7 +16,14 @@ $conn =new mysqli($server, $username, $password, $database);
 
 // Check connection
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    ///die("Connection failed: " . mysqli_connect_error());
+    $server = "iquipe.heliohost.org";
+    $username = "iquipe_castporta";
+    $password = "@passWD8282";
+    $database = "iquipe_cash_portal";
+    $conn =new mysqli($server, $username, $password, $database);
+
+
 }else{
     session_start();
 }
